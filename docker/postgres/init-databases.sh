f98da3249e3776e730461b15f6d70d5e5e9602d4
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-05-28 13:41:51 +0300

    refactor: enforce t_platform schema creation

--- docker/postgres/init-databases.sh
+++ docker/postgres/init-databases.sh
@@ -4,14 +4,14 @@ set -e
 # Create IAM user and database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER svc_iam_dba WITH PASSWORD 'svc_iam_dba';
-    CREATE DATABASE iam;
-    GRANT ALL PRIVILEGES ON DATABASE iam TO svc_iam_dba;
+    CREATE DATABASE iamservice;
+    GRANT ALL PRIVILEGES ON DATABASE iamservice TO svc_iam_dba;
 EOSQL
 
 # Run Billing Service initialization
-echo "Running initialization for 'billing' database..."
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "billing" -f /docker-entrypoint-initdb.d/init-billing.sql
+echo "Running initialization for 'billingservice' database..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "billingservice" -f /docker-entrypoint-initdb.d/init-billing.sql
 
 # Run IAM Service initialization
-echo "Running initialization for 'iam' database..."
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "iam" -f /docker-entrypoint-initdb.d/init-iam.sql
+echo "Running initialization for 'iamservice' database..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "iamservice" -f /docker-entrypoint-initdb.d/init-iam.sql
